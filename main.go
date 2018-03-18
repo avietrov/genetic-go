181f16f5f1fe9ec82315bb65df1558bf09e98cd7
Anton Vietrov <anton.vietrov@zalando.de>
2018-03-18 12:50:45 +0100

    Rework config loading.

--- main.go
+++ main.go
@@ -2,41 +2,34 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 
 	"github.com/avietrov/genetic-go/genimg"
 )
 
-func main() {
-	targetPath := flag.String("target", "target.png", "target image")
-	conf := readConfig()
-	flag.Parse()
-	target, err := genimg.ReadTarget(*targetPath)
-	if err != nil {
-		log.Fatal(err)
-	}
-	w := target.Width()
-	h := target.Height()
+var xoverC int
+var populS int
+var genomS int
+var mutatP float64
+var mutatC int
+var maxGen int
+var target string
 
-	genimg.FindFittest(conf, genimg.FitnessTo(target), genimg.Observer(w, h))
-}
-
-func readConfig() genimg.Config {
-	var xoverC int
-	var populS int
-	var genomS int
-	var mutatP float64
-	var mutatC int
-	var maxGen int
-
+func init() {
 	flag.IntVar(&xoverC, "x-over-count", 2, "number of crossovers done per generation.")
 	flag.IntVar(&populS, "population-size", 30, "number of individuals per generation (how many images per generation).")
 	flag.IntVar(&genomS, "genom-size", 300, "how large is DNA of each individ (how many triangles per image).")
 	flag.Float64Var(&mutatP, "mutation-power", 0.2, "value from 0.0+ to to 1.0, that defines stregth of mutations.")
 	flag.IntVar(&mutatC, "mutation-count", 5, "value from 1 to {genom-size} that defines how many genes are mutated per individ mutation.")
 	flag.IntVar(&maxGen, "max-generations", 100000, "maximum number of generations.")
+	flag.StringVar(&target, "target", "target.png", "target image")
+}
 
-	return genimg.Config{
+func main() {
+	flag.Parse()
+
+	conf := genimg.Config{
 		XOverCount:     xoverC,
 		PopulSize:      populS,
 		GenomSize:      genomS,
@@ -44,4 +37,14 @@ func readConfig() genimg.Config {
 		GenesToMutate:  mutatC,
 		MaxGenerations: maxGen,
 	}
+
+	fmt.Printf("Staring with config: %#v\n", conf)
+	targetImg, err := genimg.ReadTarget(target)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w := targetImg.Width()
+	h := targetImg.Height()
+
+	genimg.FindFittest(conf, genimg.FitnessTo(targetImg), genimg.Observer(w, h))
 }
